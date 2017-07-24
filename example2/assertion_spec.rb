3e85d04d19cdcf38a4e0b14ae03d35e4c1fc8c4c
ACzero <lzh.scut@hotmail.com>
2017-07-24 10:19:25 +0800

    modified example

--- example2/assertion_spec.rb
+++ example2/assertion_spec.rb
@@ -6,10 +6,14 @@ require "#{current_dir}/../spec/spec_helper.rb"
 
 RSpec.describe 'Assertion' do
   describe 'expect equality' do
-    it { expect(1).to eq(1) }
     it do
-      arr = [1, 2, 3]
-      expect(arr).not_to equal([1, 2, 3])
+      foo = 1
+      expect(foo).to eq(1)
+    end
+
+    it do
+      foo = [1, 2, 3]
+      expect(foo).not_to equal([1, 2, 3])
     end
   end
 
@@ -26,7 +30,14 @@ RSpec.describe 'Assertion' do
 
   describe 'exception' do
     it do
-      expect {  nil.split(',') }.to raise_error(NoMethodError)
+      expect { nil.split(',') }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe 'state change' do
+    it do
+      arr = [1]
+      expect { arr += [2, 3] }.to change { arr.size }.by(2)
     end
   end
 end
